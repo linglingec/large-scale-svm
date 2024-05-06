3438d01f444d4b9a21e0af6dc27251fe5f29693e
geiert01 <StabiloBoss2909!>
2024-05-06 15:55:47 +0200

    utils: add documentation

--- utils.py
+++ utils.py
@@ -4,35 +4,91 @@ import scipy
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import KFold
 
+
 def get_data(dataset):
-    if dataset == 'tiny':
-        data = pd.read_csv('data/toydata_tiny.csv')
+    """
+    Load and preprocess the specified dataset.
+
+    Parameters:
+    -----------
+    dataset : str
+        Name of the dataset ('tiny', 'large', or 'imdb').
+
+    Returns:
+    -----------
+    tuple or tuple of numpy.ndarrays
+        Depending on the dataset:
+        - For 'tiny' and 'large' datasets, returns X and y.
+        - For 'imdb' dataset, returns X_train, X_test, y_train, and y_test.
+    """
+    if dataset == "tiny":
+        data = pd.read_csv("data/toydata_tiny.csv")
         X = data.iloc[:, :-1].values
         y = data.iloc[:, -1].values
 
         return X, y
 
-    if dataset == 'large':
-        data = pd.read_csv('data/toydata_large.csv')
+    if dataset == "large":
+        data = pd.read_csv("data/toydata_large.csv")
         X = data.iloc[:, :-1].values
         y = data.iloc[:, -1].values
 
         return X, y
 
-    if dataset == 'imdb':
-        data = np.load('data/imdb.npz', allow_pickle=True)
+    if dataset == "imdb":
+        data = np.load("data/imdb.npz", allow_pickle=True)
 
-        X_train = data['train']
-        y_train = data['train_labels']
-        X_test = data['test']
-        y_test = data ['test_labels']
+        X_train = data["train"]
+        y_train = data["train_labels"]
+        X_test = data["test"]
+        y_test = data["test_labels"]
 
         X_train = X_train.item().todense()
         X_test = X_test.item().todense()
-        
+
         return X_train, X_test, y_train, y_test
 
-def grid_search_svm(X, y, model, optimizer, lambdas, learning_rates, epochs=30, batch_size=10, n_splits=5):
+
+def grid_search_svm(
+    X,
+    y,
+    model,
+    optimizer,
+    lambdas,
+    learning_rates,
+    epochs=30,
+    batch_size=10,
+    n_splits=5,
+):
+    """
+    Perform grid search for the best hyperparameters of SVM.
+
+    Parameters:
+    -----------
+    X : numpy.ndarray
+        Features.
+    y : numpy.ndarray
+        Target labels.
+    model : object
+        SVM model object.
+    optimizer : object
+        Optimizer object.
+    lambdas : list
+        List of regularization parameters.
+    learning_rates : list
+        List of learning rates.
+    epochs : int, optional (default=30)
+        Number of epochs for training.
+    batch_size : int, optional (default=10)
+        Size of mini-batch for training.
+    n_splits : int, optional (default=5)
+        Number of folds for cross-validation.
+
+    Returns:
+    -----------
+    tuple
+        Best hyperparameters, best accuracy, and dictionary of results.
+    """
     kf = KFold(n_splits=n_splits)
     results = {}
 
@@ -56,7 +112,35 @@ def grid_search_svm(X, y, model, optimizer, lambdas, learning_rates, epochs=30, 
     best_params = max(results, key=results.get)
     return best_params, results[best_params], results
 
+
 def cross_val_score(X, y, model, optimizer, epochs=30, batch_size=10, n_splits=5):
+    """
+    Perform cross-validation and calculate average accuracy.
+
+    Parameters:
+    -----------
+    X : numpy.ndarray
+        Features.
+    y : numpy.ndarray
+        Target labels.
+    model : object
+        SVM model object.
+    optimizer : object
+        Optimizer object.
+    epochs : int, optional (default=30)
+        Number of epochs for training.
+    batch_size : int, optional (default=10)
+        Size of mini-batch for training.
+    n_splits : int, optional (default=5)
+        Number of folds for cross-validation.
+
+    Returns:
+    -----------
+    float
+        Average accuracy.
+    list
+        List of loss histories for each fold.
+    """
     kf = KFold(n_splits=n_splits)
     accuracies = []
     all_loss_histories = []
